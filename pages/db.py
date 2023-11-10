a8e61460bd8e2ba158d21b6e987bf6685ffd9b63
ultrablob <neelgillshah@gmail.com>
2023-11-09 20:56:11 -0500

    database gaming

--- pages/db.py
+++ pages/db.py
@@ -5,15 +5,17 @@ from redis.commands.search.query import Query
 
 r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 
-schema = (
-    TextField("$.title", as_name="title"),
-    TextField("$.description", as_name="description"),
-    TextField("$.content", as_name="content"),
-    TextField("$.url", as_name="url")
-)
+def create_schema():
+    schema = (
+        TextField("$.title", as_name="title"),
+        TextField("$.content", as_name="content"),
+        TextField("$.url", as_name="url")
+    )
 
-index = r.ft("idx:docs")
-index.create_index(
-    schema,
-    definition=IndexDefinition(prefix=["docs:"], index_type=IndexType.JSON),
-)
+    index = r.ft("idx:docs")
+    index.create_index(
+        schema,
+        definition=IndexDefinition(prefix=["docs:"], index_type=IndexType.JSON),
+    )
+
+create_schema()
