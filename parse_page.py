125343f41f6c877608ba9638fa1e4f4f0eb8e9c6
Ultrablob <neelgillshah@gmail.com>
2023-11-15 09:36:13 -0500

    removed some unecessary files

--- parse_page.py
+++
@@ -1,6 +0,0 @@
-from lxml import html
-
-tree = html.parse("test.html")
-
-for component in tree.xpath("//div[@class='template-components__group']"):
-    print(component.text_content().replace(" ", "").replace("\n", ""))
